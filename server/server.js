22fe0b874b4eb029b708004c2de4dfa904c638d4
Mendrika <mendrikarkt18@gmail.com>
2024-07-18 03:14:09 +0300

    configure access to mongoose

--- server/server.js
+++ server/server.js
@@ -19,12 +19,11 @@ const io = socketIo(httpServer, {
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => console.log("MongoDB Connected..."))
-.catch((err) => console.log('Could not connect to MongoDB',err));
+const mongoURI = process.env.MONGO_URI;
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 // Import routes
 const authRoutes = require('./routes/auth');
